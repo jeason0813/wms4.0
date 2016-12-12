452a659d843b5a40b7e0fcc7e0985f093e9d1398
ZhangYue <254063173@qq.com>
2016-12-12 17:27:22 +0800

    12日下班

--- IBLL/IMenuService.cs
+++ IBLL/IMenuService.cs
@@ -8,6 +8,8 @@ namespace IBLL
 {
     public partial interface IMenuService : IBaseService<Menu>
     {
-         List<MenuView> GetData(string UserCode);
+        List<MenuView> GetOnesMenu(string userCode);
+        List<MenuView> GetData(int RoleCode);
+        string SaveRoleMenu(int RoleId, int[] checkListId, int[] indeterminateListId);
     }
 }
---
+++ IBLL/IRoleService.cs
@@ -0,0 +1,14 @@
+﻿using DBModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IBLL
+{
+    public  partial interface IRoleService: IBaseService<Role>
+    {
+         IQueryable<Role> GetOnesRole(string userCode);
+        string SaveRoles(string UserCode, int[] Roles);
+    }
+}
