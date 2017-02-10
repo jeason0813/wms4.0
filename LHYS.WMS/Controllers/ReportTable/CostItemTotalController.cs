10e75926fcb6bf89d1c906ae2ae426401236051c
ZhangYue <254063173@qq.com>
2017-02-10 11:21:52 +0800

    费用项目汇总表、明细表 导出

--- LHYS.WMS/Controllers/ReportTable/CostItemTotalController.cs
+++ LHYS.WMS/Controllers/ReportTable/CostItemTotalController.cs
@@ -1,6 +1,10 @@
-﻿using IBLL;
+﻿using Common;
+using IBLL;
+using LHYS.WMS.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -58,15 +62,79 @@ namespace LHYS.WMS.Controllers
                 var temp = from a in res
                            group a by new { a.CostItemCode, a.CostItemName } into g
                            select new { g.Key.CostItemCode, g.Key.CostItemName, Price = g.Sum(a => a.AllAmount) };
-                var tempNew= temp.ToList().Select(a => new { a.CostItemCode, a.CostItemName, a.Price, Type = a.CostItemCode.Substring(0, 2) == "64" ? "成本" : "收入" });
+                var tempNew = temp.ToList().Select(a => new { a.CostItemCode, a.CostItemName, a.Price, Type = a.CostItemCode.Substring(0, 2) == "64" ? "成本" : "收入" });
                 int count = tempNew.Count();
                 //分页
-                var result= tempNew.OrderBy(a=>a.CostItemCode).Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
+                var result = tempNew.OrderBy(a => a.CostItemCode).Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
                 return Json(new
                 {
                     data = result,
                     totalCount = count
                 });
+            }
+        }
+
+        public ActionResult ExportTable(DateTime? timestart, DateTime? timeend, string DepartmentId)
+        {
+            if (Session["Power"] == null || Session["UserCode"] == null)
+            {
+                return null;
+            }
+            else
+            {
+                string Power = Session["Power"].ToString();//权限字符串
+                //获取装卸费力资费明细
+                var list1 = LoadingAndLaborCostDetailService.LoadEntities(a => Power.Contains(a.DepartmentId)).ToList();
+                if (timestart != null)
+                {
+                    list1 = list1.Where(a => a.ExaminaDate >= timestart).ToList();
+                }
+                if (timeend != null)
+                {
+                    list1 = list1.Where(a => a.ExaminaDate < timeend.GetValueOrDefault().AddDays(1)).ToList();
+                }
+                //获取其他明细
+                var list2 = LoadingAndLaborCostDetailService.LoadEntities(a => Power.Contains(a.DepartmentId)).ToList();
+                if (timestart != null)
+                {
+                    list2 = list2.Where(a => a.ExaminaDate >= timestart).ToList();
+                }
+                if (timeend != null)
+                {
+                    list2 = list2.Where(a => a.ExaminaDate < timeend.GetValueOrDefault().AddDays(1)).ToList();
+                }
+                //合并list1  list2   提取有用字段  返回
+                var res = list1.Select(a => new { a.CostItemCode, a.CostItemName, a.AllAmount }).ToList();
+                var res2 = list2.Select(a => new { a.CostItemCode, a.CostItemName, a.AllAmount }).ToList();
+                //合并成一个list
+                res.AddRange(res2);
+                //分组求和
+                var temp = from a in res
+                           group a by new { a.CostItemCode, a.CostItemName } into g
+                           select new { g.Key.CostItemCode, g.Key.CostItemName, Price = g.Sum(a => a.AllAmount) };
+                var tempNew = temp.ToList().Select(a => new { a.CostItemCode, a.CostItemName, a.Price, Type = a.CostItemCode.Substring(0, 2) == "64" ? "成本" : "收入" });
+                //转换成DataTable
+                DataTable dt = new DataTable();
+                dt.Columns.Add("科目编码");
+                dt.Columns.Add("科目名称");
+                dt.Columns.Add("科目分类");
+                dt.Columns.Add("发生金额");
+                foreach (var item in tempNew)
+                {
+                    DataRow dr = dt.NewRow();
+                    dr["科目编码"] = item.CostItemCode;
+                    dr["科目名称"] = item.CostItemName;
+                    dr["科目分类"] = item.Type;
+                    dr["发生金额"] = item.Price;
+                    dt.Rows.Add(dr);
+                }
+                MemoryStream file = NPIOHelper.RenderToMemory(dt, "sheet1");
+                Response.Charset = "UTF-8";
+                Response.ContentEncoding = System.Text.Encoding.GetEncoding("UTF-8");
+                Response.ContentType = "application / vnd.openxmlformats - officedocument.spreadsheetml.sheet";// "application/ms-excel/msword";
+                Response.AddHeader("Content-Disposition", "attachment;fileName=费用项目汇总表.xls");
+                Response.BinaryWrite(file.ToArray());
+                return new EmptyResult();
             }
         }
     }
--- LHYS.WMS/Controllers/ReportTable/CostTotalController.cs
+++ LHYS.WMS/Controllers/ReportTable/CostTotalController.cs
@@ -1,6 +1,9 @@
-﻿using IBLL;
+﻿using Common;
+using IBLL;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -17,7 +20,7 @@ namespace LHYS.WMS.Controllers
             return View();
         }
         //查询记录
-        public ActionResult Search(int pageIndex,int pageSize,DateTime? timestart,DateTime? timeend, string DepartmentId,string CostItemCode)
+        public ActionResult Search(int pageIndex, int pageSize, DateTime? timestart, DateTime? timeend, string DepartmentId, string CostItemCode)
         {
             if (Session["Power"] == null || Session["UserCode"] == null)
             {
@@ -28,7 +31,7 @@ namespace LHYS.WMS.Controllers
                 string UserCode = Session["UserCode"].ToString();//员工编号
                 string Power = Session["Power"].ToString();//权限字符串
                 var list = CostTotalService.LoadEntities(a => Power.Contains(a.DepartmentId));
-                if (CostItemCode != null)
+                if (CostItemCode != null&&CostItemCode!="")
                 {
                     list = list.Where(a => a.CostItemCode == CostItemCode);
                 }
@@ -41,8 +44,9 @@ namespace LHYS.WMS.Controllers
                     list = list.Where(a => a.BillDate < timeend.GetValueOrDefault().AddDays(1));
                 }
                 int count = list.Count();
-                list = list.OrderBy(a=>a.OrderNumber).ThenBy(a=>a.BillCode).Skip(pageSize * (pageIndex - 1)).Take(pageSize);
-                return Json(new {
+                list = list.OrderBy(a => a.OrderNumber).ThenBy(a => a.BillCode).Skip(pageSize * (pageIndex - 1)).Take(pageSize);
+                return Json(new
+                {
                     data = list,
                     totalCount = count
                 });
@@ -69,6 +73,64 @@ namespace LHYS.WMS.Controllers
                 return Json(res);
             }
 
+        }
+
+        public ActionResult ExportTable(DateTime? timestart, DateTime? timeend, string DepartmentId, string CostItemCode)
+        {
+            if (Session["Power"] == null || Session["UserCode"] == null)
+            {
+                return null;
+            }
+            else
+            {
+                string UserCode = Session["UserCode"].ToString();//员工编号
+                string Power = Session["Power"].ToString();//权限字符串
+                var list = CostTotalService.LoadEntities(a => Power.Contains(a.DepartmentId));
+                if (CostItemCode != null&& CostItemCode!="")
+                {
+                    list = list.Where(a => a.CostItemCode == CostItemCode);
+                }
+                if (timestart != null)
+                {
+                    list = list.Where(a => a.BillDate >= timestart);
+                }
+                if (timeend != null)
+                {
+                    list = list.Where(a => a.BillDate < timeend.GetValueOrDefault().AddDays(1));
+                }
+                //生成Datatable
+                DataTable dt = new DataTable();
+                dt.Columns.Add("单据类型");
+                dt.Columns.Add("单据编码");
+                dt.Columns.Add("单据日期");
+                dt.Columns.Add("制单人");
+                dt.Columns.Add("审核人");
+                dt.Columns.Add("费用编码");
+                dt.Columns.Add("费用项目");
+                dt.Columns.Add("金额");
+                dt.Columns.Add("备注");
+                foreach (var item in list)
+                {
+                    DataRow dr = dt.NewRow();
+                    dr["单据类型"] = item.billType;
+                    dr["单据编码"] = item.BillCode;
+                    dr["单据日期"] = item.BillDate;
+                    dr["制单人"] = item.MakePerson;
+                    dr["审核人"] = item.ExaminePerson;
+                    dr["费用编码"] = item.CostItemCode;
+                    dr["费用项目"] = item.CostItemName;
+                    dr["金额"] = item.AllAmount;
+                    dr["备注"] = item.Remark;
+                    dt.Rows.Add(dr);
+                }
+                MemoryStream file = NPIOHelper.RenderToMemory(dt, "sheet1");
+                Response.Charset = "UTF-8";
+                Response.ContentEncoding = System.Text.Encoding.GetEncoding("UTF-8");
+                Response.ContentType = "application / vnd.openxmlformats - officedocument.spreadsheetml.sheet";// "application/ms-excel/msword";
+                Response.AddHeader("Content-Disposition", "attachment;fileName=费用项目明细表.xls");
+                Response.BinaryWrite(file.ToArray());
+                return new EmptyResult();
+            }
         }
     }
 }
